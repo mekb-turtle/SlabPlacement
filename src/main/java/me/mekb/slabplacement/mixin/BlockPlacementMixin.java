3131f69b328f9636eaea506363aa7d9ffd2948c9
mekb <mekbgithub@icloud.com>
2023-10-23 23:56:27 +1100

    :bug: Allow placing on top of bottom slab, and below top slab, instead of replacing the slab

--- src/main/java/me/mekb/slabplacement/mixin/BlockPlacementMixin.java
+++ src/main/java/me/mekb/slabplacement/mixin/BlockPlacementMixin.java
@@ -8,10 +8,10 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.Waterloggable;
 import net.minecraft.block.enums.SlabType;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -54,5 +54,25 @@ public class BlockPlacementMixin extends Block implements Waterloggable {
                 .with(TYPE, slabType);
 
         cir.setReturnValue(newBlockState);
+    }
+
+    @Inject(method = "canReplace(Lnet/minecraft/block/BlockState;Lnet/minecraft/item/ItemPlacementContext;)Z", at = @At("HEAD"), cancellable = true)
+    private void injectCanReplace(BlockState state, ItemPlacementContext ctx, CallbackInfoReturnable<Boolean> cir) {
+        SlabPlacementMode slabPlacementMode = SlabPlacementMode.DEFAULT;
+
+        if (ctx.getWorld().isClient) {
+            if (SlabPlacementClient.getIsServerInitialized()) {
+                slabPlacementMode = SlabPlacementClient.slabPlacementMode;
+            }
+        } else {
+            slabPlacementMode = SlabPlacement.getSlabPlacementMode(ctx.getPlayer());
+        }
+
+        if (slabPlacementMode == SlabPlacementMode.DEFAULT) return;
+
+        ItemStack itemStack = ctx.getStack();
+        if (itemStack.isOf(this.asItem())) {
+            cir.setReturnValue(false);
+        }
     }
 }
